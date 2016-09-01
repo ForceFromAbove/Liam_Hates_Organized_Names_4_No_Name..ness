4effd51feaa9a4ef3a23a27ab13c2f243c77de11
Aaron Ewing <aaronewing@nnu.edu>
2016-09-01 13:26:18 -0600

    ch 2 example 2.15

---
+++ src/Liam_Hates_Organized_Names_4_No_Nameness.java
@@ -0,0 +1,15 @@
+/*
+ * Example 2.15
+*/
+
+public class Liam_Hates_Organized_Names_4_No_Nameness {
+    public static void main( String[] args ){
+        System.out.println("blah");
+    }
+}
+
+
+
+/**
+ * Created by AaronEwing on 9/1/2016.
+ */
